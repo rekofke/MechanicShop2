fa8cf5f17d804adcaf9539fc02d5c991daa70a50
rekofke <rekofke.eric@gmail.com>
2025-06-09 19:44:30 -0700

    adjusted swagger

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from app import create_app
 from app.models import db
+from flask import redirect
 
 
 app = create_app('ProductionConfig')
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect('/api/docs')
 
 
 with app.app_context():
